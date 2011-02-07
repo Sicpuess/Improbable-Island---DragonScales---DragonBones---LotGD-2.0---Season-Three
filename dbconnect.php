bc99ec6c8793d7c06eca2cdeee061063254455bb
Sicpuess <sicpuess@gmail.com>
2011-02-07 17:39:49 +0100

    added dbconnect file

---
+++ dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+//This file automatically created by installer.php on Apr 25, 2008 04:30 pm
+$DB_HOST = 'localhost';
+$DB_USER = 'root';
+$DB_PASS = 'parole';
+$DB_NAME = 'labs';
+$DB_PREFIX = 'free_';
+$DB_USEDATACACHE = 0;
+$DB_DATACACHEPATH = 'datacache';
+?>
